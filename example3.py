ab3036a61a0f65c721a6bff07f9b57406ec82e60
twobeers75 <johntoubia@bigpond.com>
2013-09-25 14:24:46 +0930

    Added comments to file

--- example3.py
+++ example3.py
@@ -1,7 +1,10 @@
+# input string to test
 input_string = "GATCAGTCGATCGACTGCTAGCTAGCTAGTACGGCGTATA"
 
+# create the dictionary by counting all occurences of A's, G's, C's and T's 
 my_dict={'A':input_string.count('A'),'C':input_string.count('C'),'G':input_string.count('G'),'T':input_string.count('T')}
 
+# print dict
 print my_dict
 
 
